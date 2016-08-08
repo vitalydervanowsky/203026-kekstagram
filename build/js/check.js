503e0930ef4b65d4858cea724c9ed4c79e92f128
Vitaly <vitalydervanowsky@gmail.com>
2016-08-08 22:37:09 +0300

    check.js после исправлений
    что именно лишнее? дважды обходить массив а (на 13 строке и на 19)?

--- build/js/check.js
+++ build/js/check.js
@@ -10,14 +10,14 @@ function getMessage(a, b) {
     message = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
   } else if (Object.prototype.toString.call( a ) === '[object Array]' && Object.prototype.toString.call( b ) !== '[object Array]') {
     var amountOfRedPoints = 0;
-    for (var i = a.length - 1; i >= 0; i--) {
+    for (var i = 0; i < a.length; i++) {
       amountOfRedPoints += a[i];
     }
     message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   } else if (Object.prototype.toString.call( a ) === '[object Array]' && Object.prototype.toString.call( b ) === '[object Array]') {
     var artifactsSquare = 0;
-    for (i = a.length - 1; i >= 0; i--) {
-      for (var j = b.length - 1; j >= 0; j--) {
+    for (i = 0; i < a.length; i++) {
+      for (var j = 0; j < b.length; j++) {
         artifactsSquare += a[i] * b[j];
       }
     }
