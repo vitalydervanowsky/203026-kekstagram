6c371f1550515fead5c8505435c5b6eef4aef6a2
vitalydervanowsky <vitalydervanowsky@gmail.com>
2016-08-25 12:54:51 +0300

    jsonp

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 'use strict';
 
+require('./pictures.js');
 require('./resizer.js');
 require('./upload.js');
---
+++ src/pictures.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var pictures = null;
+
+var loadPictures = function(address, callback) {
+  var scriptEl = document.createElement('script');
+  scriptEl.src = address;
+  document.body.appendChild(scriptEl);
+
+  window.JSONPCallback = function(data) {
+    callback(data);
+  };
+};
+
+loadPictures('http://localhost:1506/api/pictures?callback=JSONPCallback', function(data) {
+  pictures = data;
+});
