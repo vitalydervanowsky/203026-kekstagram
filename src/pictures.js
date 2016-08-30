2121f69f9a14b849ddf7cb331ad26071298da00f
vitalydervanowsky <vitalydervanowsky@gmail.com>
2016-08-30 15:17:38 +0300

    amd v.0.3

--- src/pictures.js
+++ src/pictures.js
@@ -5,21 +5,18 @@ define('pictures',
     './load-pictures',
     './get-picture-element'
   ], function(loadPictures, getPictureElement) {
-    // console.log(loadPictures);
-    return function() {
+    (function() {
       var picturesContainer = document.querySelector('.pictures');
-      console.log('111');
 
-      window.loadPictures('http://localhost:1506/api/pictures?callback=JSONPCallback', function(data) {
+      loadPictures('http://localhost:1506/api/pictures?callback=JSONPCallback', function(data) {
         var filtersBlock = document.querySelector('.filters');
         filtersBlock.classList.add('hidden');
         var pictures = data;
-        // console.log(pictures);
         pictures.forEach(function(pic) {
-          window.getPictureElement(pic, picturesContainer);
+          getPictureElement(pic, picturesContainer);
         });
 
         filtersBlock.classList.remove('hidden');
       });
-    };
+    })();
   });
