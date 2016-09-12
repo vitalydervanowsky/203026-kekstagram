079035807a4320699bea1c333095bdb7218f601d
vitalydervanowsky <vitalydervanowsky@gmail.com>
2016-09-12 11:22:46 +0300

    Постраничный вывод и делегирование событий

--- src/pictures.js
+++ src/pictures.js
@@ -4,17 +4,65 @@ define('pictures',
   ['./load', './picture', './gallery'],
   function(load, Picture, gallery) {
     (function() {
-      var picturesContainer = document.querySelector('.pictures');
+      var PICTURES_LOAD_URL = '/api/pictures';
+      var THROTTLE_TIMEOUT = 100;
+      var GAP = 100;
 
-      load('http://localhost:1506/api/pictures?callback=JSONPCallback', function(data) {
-        var filtersBlock = document.querySelector('.filters');
-        filtersBlock.classList.add('hidden');
-        var pictures = data;
-        pictures.forEach(function(picData, index) {
+      var activeFilter = 'filter-popular';
+      var picturesContainer = document.querySelector('.pictures');
+      var filtersBlock = document.querySelector('.filters');
+      filtersBlock.classList.remove('hidden');
+      var footer = document.querySelector('footer');
+      var pageNumber = 0;
+      var pageSize = 12;
+      var pictures = [];
+
+      var renderPictures = function(loadedPictures) {
+        loadedPictures.forEach(function(picData, index) {
           picturesContainer.appendChild(new Picture(picData, index).element);
         });
-        filtersBlock.classList.remove('hidden');
+        // console.log(loadedPictures);
+        pictures.push.apply(pictures, loadedPictures);
+        console.log(pictures);
         gallery.setPictures(pictures);
+      };
+
+      var loadPictures = function(filter, currentPageNumber) {
+        load(PICTURES_LOAD_URL, {
+          from: currentPageNumber * pageSize,
+          to: currentPageNumber * pageSize + pageSize,
+          filter: filter
+        }, renderPictures);
+      };
+
+      var changeFilter = function(filterID) {
+        picturesContainer.innerHTML = '';
+        pictures = [];
+        activeFilter = filterID;
+        pageNumber = 0;
+        loadPictures(filterID, pageNumber);
+      };
+
+      filtersBlock.addEventListener('change', function(evt) {
+        if (evt.target.classList.contains('filters-radio')) {
+          // console.log('1');
+          changeFilter(evt.target.id);
+        }
       });
+
+      var lastCall = Date.now();
+
+      window.addEventListener('scroll', function() {
+        if (Date.now() - lastCall >= THROTTLE_TIMEOUT) {
+          if (footer.getBoundingClientRect().bottom - window.innerHeight <= GAP) {
+            loadPictures(activeFilter, ++pageNumber);
+            // console.log(loadedPictures);
+          }
+
+          lastCall = Date.now();
+        }
+      });
+
+      changeFilter(activeFilter);
     })();
   });
